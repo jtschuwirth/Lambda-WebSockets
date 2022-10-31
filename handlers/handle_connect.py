0863911f6b26a7148612ba940a9d7c9d3d766650
jtschuwirth <jtschuwirth@uc.cl>
2022-10-31 14:30:17 -0300

    added basic connect and disconnect handlers, added sendmessage endpoint for basic chat functionality

--- handlers/handle_connect.py
+++ handlers/handle_connect.py
@@ -1,2 +1,24 @@
+import json
+
+from botocore.exceptions import ClientError
+from auxiliary_functions.handle_ws_message import handle_ws_message
+from auxiliary_functions.get_all_recipients import get_all_recipients
+
 def handle_connect(table, event, connection_id, apig_management_client):
-    pass
+    status_code = 200
+
+    user_name = event.get('queryStringParameters', {'name': 'guest'}).get('name')
+    room_id = event.get('queryStringParameters', {'room': "aaaa"}).get("room")
+    
+    try:
+        table.put_item(Item={
+            'connection_id': connection_id,
+            'room_id': room_id,
+            'user_name': user_name,
+        })
+        message = json.dumps({"new_connection":{"id": connection_id, "user_name": user_name}})
+        recipients = get_all_recipients(table, room_id)
+        handle_ws_message(table, recipients, message, apig_management_client)
+    except ClientError:
+        status_code = 503
+    return status_code
--- handlers/routes.py
+++ handlers/routes.py
@@ -1,8 +1,10 @@
 #Import handlers
 
+from handlers.handle_message import handle_message
+
 #Add routes and handlers to the dict {"route_key": handler_function}
 route_dic = {
-
+    "sendmessage", handle_message
 }
 
 def routes(route_key, table, event, connection_id, apig_management_client):
