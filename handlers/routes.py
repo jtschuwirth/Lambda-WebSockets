13ea4147a4248b12b9b4af6175d537ea684e14cd
jtschuwirth <jtschuwirth@uc.cl>
2023-05-13 16:42:48 -0400

    fix typo

--- handlers/routes.py
+++ handlers/routes.py
@@ -4,7 +4,7 @@ from handlers.handle_message import handle_message
 
 #Add routes and handlers to the dict {"route_key": handler_function}
 route_dic = {
-    "sendmessage", handle_message
+    "sendmessage": handle_message
 }
 
 def routes(route_key, table, event, connection_id, apig_management_client):
